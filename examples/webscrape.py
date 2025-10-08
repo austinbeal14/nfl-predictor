f41649ead1fdd544a92b3bd002825edcf376849a
austinbeal1408 <alb049@bucknell.edu>
2025-10-07 21:12:31 -0400

    Updated function name

--- examples/webscrape.py
+++ examples/webscrape.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-def scrape():
+def example_data_fetch():
     url = 'https://www.pro-football-reference.com/years/2025/games.htm'
     data = pd.read_html(url, flavor='html5lib')
     print(data)
 
 if __name__ == "__main__":
-    scrape()
+    example_data_fetch()
